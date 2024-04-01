6d81a84dbe6509f55366401bcddba7ab0a3569e8
KirthanKumar <118553301+KirthanKumar@users.noreply.github.com>
2024-04-01 17:31:44 +0530

    #69 - Adding Login Component to iNotebook

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -69,6 +69,7 @@ router.post(
     }),
   ],
   async (req, res) => {
+    let success = false;
     //   if there are errors return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -79,6 +80,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
+        success = false;
         return res
           .status(400)
           .json({ error: "Please try to login with correct credentials" });
@@ -94,7 +96,8 @@ router.post(
 
       const data = { user: { id: user.id } };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json(authtoken);
+      success = true;
+      res.json({ success, authtoken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Internal Server Error");
